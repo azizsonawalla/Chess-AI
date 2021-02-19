99839daeccf5bf65e4984f115e6b2534ce29804a
Cynzy <cynthiazhou98@gmail.com>
2021-02-18 19:02:41 -0800

    working print chessboard string

--- src/Chess/ChessBoard.hs
+++ src/Chess/ChessBoard.hs
@@ -3,12 +3,13 @@ module ChessBoard where
 import ChessPieces
 import ChessUtilTypes
 import FENotation
+import Data.Maybe
 import Debug.Trace
 
 
 -- -- Define Show for a ChessBoard
--- instance Show ChessBoard where
---     show board = chessBoardAsString board
+instance Show ChessBoard where
+    show board = chessBoardAsString board
 
 
 -- A fresh Chess Board with all the pieces in the starting position
@@ -16,17 +17,108 @@ freshBoard = fenToChessBoard "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 
 
 -- Returns the string representation of a chessboard
--- TODO: implement + test this (2 hours). [Cynthia]
 -- Note: one way to implement this is to have a 'template' string (possibly stored in a txt file) and then simply
 --       replace a placeholder in each square with the piece that is currently there.
 testboard = (ChessBoard [ (('A', 1), Rook White) ] Ongoing)
 
-chessBoardAsString :: ChessBoard -> IO ()
-chessBoardAsString (ChessBoard [(position, piece)] _) = 
+chessBoardAsString :: ChessBoard -> [Char]
+chessBoardAsString chessBoard = 
     do
-        putStr board
+        trace ("printing: \n") updatedBoard
         where
-            empty = "[  ]"
+        --  process and print board
+            updatedBoard   =    "\n*--*--* Current Chess Board *--*--*\n\n"
+                        ++ "     (1)(2)(3)(4)(5)(6)(7)(8) \n\n" 
+                        ++ "(A)  "  ++ retrievePieceAt ('A',1) chessBoard
+                                    ++ retrievePieceAt ('A',2) chessBoard 
+                                    ++ retrievePieceAt ('A',3) chessBoard 
+                                    ++ retrievePieceAt ('A',4) chessBoard 
+                                    ++ retrievePieceAt ('A',5) chessBoard 
+                                    ++ retrievePieceAt ('A',6) chessBoard 
+                                    ++ retrievePieceAt ('A',7) chessBoard 
+                                    ++ retrievePieceAt ('A',8) chessBoard 
+                        ++ "\n" 
+                        ++ "(B)  "  ++ retrievePieceAt ('B',1) chessBoard
+                                    ++ retrievePieceAt ('B',2) chessBoard 
+                                    ++ retrievePieceAt ('B',3) chessBoard 
+                                    ++ retrievePieceAt ('B',4) chessBoard 
+                                    ++ retrievePieceAt ('B',5) chessBoard 
+                                    ++ retrievePieceAt ('B',6) chessBoard 
+                                    ++ retrievePieceAt ('B',7) chessBoard 
+                                    ++ retrievePieceAt ('B',8) chessBoard
+                        ++ "\n"
+                        ++ "(C)  "  ++ retrievePieceAt ('C',1) chessBoard
+                                    ++ retrievePieceAt ('C',2) chessBoard 
+                                    ++ retrievePieceAt ('C',3) chessBoard 
+                                    ++ retrievePieceAt ('C',4) chessBoard 
+                                    ++ retrievePieceAt ('C',5) chessBoard 
+                                    ++ retrievePieceAt ('C',6) chessBoard 
+                                    ++ retrievePieceAt ('C',7) chessBoard 
+                                    ++ retrievePieceAt ('C',8) chessBoard 
+                        ++ "\n"
+                        ++ "(D)  "  ++ retrievePieceAt ('D',1) chessBoard
+                                    ++ retrievePieceAt ('D',2) chessBoard 
+                                    ++ retrievePieceAt ('D',3) chessBoard 
+                                    ++ retrievePieceAt ('D',4) chessBoard 
+                                    ++ retrievePieceAt ('D',5) chessBoard 
+                                    ++ retrievePieceAt ('D',6) chessBoard 
+                                    ++ retrievePieceAt ('D',7) chessBoard 
+                                    ++ retrievePieceAt ('D',8) chessBoard
+                        ++ "\n"
+                        ++ "(E)  "  ++ retrievePieceAt ('E',1) chessBoard
+                                    ++ retrievePieceAt ('E',2) chessBoard 
+                                    ++ retrievePieceAt ('E',3) chessBoard 
+                                    ++ retrievePieceAt ('E',4) chessBoard 
+                                    ++ retrievePieceAt ('E',5) chessBoard 
+                                    ++ retrievePieceAt ('E',6) chessBoard 
+                                    ++ retrievePieceAt ('E',7) chessBoard 
+                                    ++ retrievePieceAt ('E',8) chessBoard
+                        ++ "\n"
+                        ++ "(F)  "  ++ retrievePieceAt ('F',1) chessBoard
+                                    ++ retrievePieceAt ('F',2) chessBoard 
+                                    ++ retrievePieceAt ('F',3) chessBoard 
+                                    ++ retrievePieceAt ('F',4) chessBoard 
+                                    ++ retrievePieceAt ('F',5) chessBoard 
+                                    ++ retrievePieceAt ('F',6) chessBoard 
+                                    ++ retrievePieceAt ('F',7) chessBoard 
+                                    ++ retrievePieceAt ('F',8) chessBoard 
+                        ++ "\n"
+                        ++ "(G)  "  ++ retrievePieceAt ('G',1) chessBoard
+                                    ++ retrievePieceAt ('G',2) chessBoard 
+                                    ++ retrievePieceAt ('G',3) chessBoard 
+                                    ++ retrievePieceAt ('G',4) chessBoard 
+                                    ++ retrievePieceAt ('G',5) chessBoard 
+                                    ++ retrievePieceAt ('G',6) chessBoard 
+                                    ++ retrievePieceAt ('G',7) chessBoard 
+                                    ++ retrievePieceAt ('G',8) chessBoard 
+                        ++ "\n"
+                        ++ "(H)  "  ++ retrievePieceAt ('H',1) chessBoard
+                                    ++ retrievePieceAt ('H',2) chessBoard 
+                                    ++ retrievePieceAt ('H',3) chessBoard 
+                                    ++ retrievePieceAt ('H',4) chessBoard 
+                                    ++ retrievePieceAt ('H',5) chessBoard 
+                                    ++ retrievePieceAt ('H',6) chessBoard 
+                                    ++ retrievePieceAt ('H',7) chessBoard 
+                                    ++ retrievePieceAt ('H',8) chessBoard
+                        ++ "\n\n" 
+
+retrievePieceAt:: ChessPosition -> ChessBoard -> [Char]
+retrievePieceAt (char,num) board
+    | getPieceAt (char,num) board == Nothing                    = empty
+    | getPieceAt (char,num) board == Just (King White)          = whiteKing
+    | getPieceAt (char,num) board == Just (Queen White)         = whiteQueen
+    | getPieceAt (char,num) board == Just (Rook White)          = whiteRook
+    | getPieceAt (char,num) board == Just (Bishop White)        = whiteBishop
+    | getPieceAt (char,num) board == Just (Knight White)        = whiteKnight
+    | getPieceAt (char,num) board == Just (Pawn White)          = whitePawn
+    | getPieceAt (char,num) board == Just (King Black)          = blackKing
+    | getPieceAt (char,num) board == Just (Queen Black)         = blackQueen
+    | getPieceAt (char,num) board == Just (Rook Black)          = blackRook
+    | getPieceAt (char,num) board == Just (Bishop Black)        = blackBishop
+    | getPieceAt (char,num) board == Just (Knight Black)        = blackKnight
+    | getPieceAt (char,num) board == Just (Pawn Black)          = blackPawn
+    where
+            empty = "[ ]"
             whiteKing = "[\x2654]"
             whiteQueen = "[\x2655]"
             whiteRook = "[\x2656]"
@@ -41,19 +133,6 @@ chessBoardAsString (ChessBoard [(position, piece)] _) =
             blackKnight = "[\x265E]"
             blackPawn = "[\x265F]"
 
-        --  process and print board
-            board   =    "\n*--*--* Current Chess Board *--*--*\n\n"
-                        ++ "     (1)(2)(3)(4)(5)(6)(7)(8) \n\n" 
-                        ++ "(A)  " ++ concat [whitePawn | i <- [1..8]] ++ "\n" 
-                        ++ "(B)  " ++ concat [whitePawn | i <- [1..8]] ++ "\n"
-                        ++ "(C)  " ++ concat [whitePawn | i <- [1..8]] ++ "\n"
-                        ++ "(D)  " ++ concat [whitePawn | i <- [1..8]] ++ "\n"
-                        ++ "(E)  " ++ concat [whitePawn | i <- [1..8]] ++ "\n"
-                        ++ "(F)  " ++ concat [whitePawn | i <- [1..8]] ++ "\n"
-                        ++ "(G)  " ++ concat [whitePawn | i <- [1..8]] ++ "\n"
-                        ++ "(H)  " ++ concat [whitePawn | i <- [1..8]] ++ "\n" 
-                        ++ "\n"
-                        
 
 -- Makes the given move on the given chessboard. Returns the board with the move made
 -- If there is a piece at the destination square, then that piece will be replaced
