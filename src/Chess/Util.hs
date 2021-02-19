4550e84b408b418aaa534c588f8db693ac51b9e5
Cynzy <cynthiazhou98@gmail.com>
2021-02-19 00:46:52 -0800

    finished stringToChessMove

--- src/Chess/Util.hs
+++
@@ -1,4 +0,0 @@
-module Util where
-
--- Returns the first element of a 3-tuple
-getFirst (a, b, c) = a
--- src/Players/HumanPlayer.hs
+++ src/Players/HumanPlayer.hs
@@ -2,42 +2,43 @@ module HumanPlayer where
 
 import ChessUtilTypes
 import ChessBoard
+import Data.Char
 
 
 -- Facilitates the human player's move on the chess board.
 -- Shows the human player the board, asks for next move, and changes the board accordingly.
 -- Returns changed board after human player's move
-humanPlayer :: ChessPlayer
-humanPlayer chessPieceColour chessBoard = 
-    do 
-        putStrLn "Your turn:"
-        putStrLn (show chessBoard)                                                  -- show the user the chess board
-        putStrLn "Enter a move (eg. c1 to d2): "                                    -- prompt for move
-        moveStr <- getLine                                                          -- get move entered by user
-        if (validMoveString moveStr)                                                -- check if string is in valid format
-        then (handleValidInput moveStr chessPieceColour chessBoard)
-        else (handleInvalidInput moveStr chessPieceColour chessBoard)
+-- humanPlayer :: ChessPlayer
+-- humanPlayer chessPieceColour chessBoard = 
+--     do 
+--         putStrLn "Your turn:"
+--         putStrLn (show chessBoard)                                                  -- show the user the chess board
+--         putStrLn "Enter a move (eg. c1 to d2): "                                    -- prompt for move
+--         moveStr <- getLine                                                          -- get move entered by user
+--         if (validMoveString moveStr)                                                -- check if string is in valid format
+--         then (handleValidInput moveStr chessPieceColour chessBoard)
+--         else (handleInvalidInput moveStr chessPieceColour chessBoard)
 
 
--- Handles the case where the human player enters an input with invalid format
-handleInvalidInput :: String -> ChessPieceColour -> ChessBoard -> IO ChessBoard
-handleInvalidInput moveStr chessPieceColour chessBoard =
-    do
-        putStrLn ("'"++moveStr++"' is an invalid format. Try again.")               -- ask human to try again
-        (humanPlayer chessPieceColour chessBoard)                                   -- retry human player protocol
+-- -- Handles the case where the human player enters an input with invalid format
+-- handleInvalidInput :: String -> ChessPieceColour -> ChessBoard -> IO ChessBoard
+-- handleInvalidInput moveStr chessPieceColour chessBoard =
+--     do
+--         putStrLn ("'"++moveStr++"' is an invalid format. Try again.")               -- ask human to try again
+--         (humanPlayer chessPieceColour chessBoard)                                   -- retry human player protocol
 
 
--- Handles the case where the human player enters an input with a valid format
-handleValidInput :: String -> ChessPieceColour -> ChessBoard -> IO ChessBoard
-handleValidInput moveStr chessPieceColour chessBoard = 
-    do
-        let move = stringToChessMove moveStr                                        -- convert string to ChessMove
-        if (validMove chessBoard chessPieceColour move)                             -- check if the move itself is valid given the current board and the player's colour
-        then do
-            return (makeMove chessBoard move)                                       -- return board after making move
-        else do
-            putStrLn ("'"++moveStr++"' is an invalid move. Try again.")             -- ask human to try again
-            (humanPlayer chessPieceColour chessBoard)                               -- recursive call to player
+-- -- Handles the case where the human player enters an input with a valid format
+-- handleValidInput :: String -> ChessPieceColour -> ChessBoard -> IO ChessBoard
+-- handleValidInput moveStr chessPieceColour chessBoard = 
+--     do
+--         let move = stringToChessMove moveStr                                        -- convert string to ChessMove
+--         if (validMove chessBoard chessPieceColour move)                             -- check if the move itself is valid given the current board and the player's colour
+--         then do
+--             return (makeMove chessBoard move)                                       -- return board after making move
+--         else do
+--             putStrLn ("'"++moveStr++"' is an invalid move. Try again.")             -- ask human to try again
+--             (humanPlayer chessPieceColour chessBoard)                               -- recursive call to player
 
 
 -- Checks if the given string has a valid format for a chess move
@@ -49,6 +50,13 @@ validMoveString str = False
 
 -- Converts a chess move from the "[a-h][1-8] to [a-h][1-8]" format to a ChessMove
 -- WARNING: Assumes that the string is in a valid format
--- TODO: Implement + test this (1.5 hour) [Cynthia]
 stringToChessMove :: String -> ChessMove
-stringToChessMove _ = ChessMove ('A', 0) ('A', 0)
+stringToChessMove string = 
+    ChessMove (stringToChessMoveHelper fromPosition) (stringToChessMoveHelper toPosition)
+    where
+        fromPosition = take 2 [ x | x <- string, x /=' ', x /='t', x/='o']
+        toPosition = drop 2 [ x | x <- string, x /=' ', x /='t', x/='o'] 
+
+stringToChessMoveHelper :: [Char] -> (Char, Integer)
+stringToChessMoveHelper string = (string !! 0, toInteger (digitToInt (string !! 1)))
+
